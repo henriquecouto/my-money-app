71a9520deded9a281c71cd493ff0fd63d5c4e0aa
Henrique Couto <henrique.coutu@live.com>
2019-04-23 02:22:53 -0300

    first commit

---
+++ backend/src/config/routes.js
@@ -0,0 +1,13 @@
+const express = require('express')
+
+module.exports = function (server) {
+
+  //URL base
+  const router = express.Router()
+  server.use('/api', router)
+
+  //Rotas de Ciclo de Pagamento
+  const BillingCycle = require('../api/billingCycle/billingCycleService')
+  BillingCycle.register(router, '/billing-cycles')
+
+}
